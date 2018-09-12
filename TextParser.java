ac9abd9df7434194ebb6a2d2f62be72593937e59
cfiorini74 <cfiorini74@gmail.com>
2018-09-11 22:03:46 -0400

    woah

--- TextParser.java
+++ TextParser.java
@@ -2,13 +2,19 @@ package RealCalculator;
 
 import java.util.ArrayList;
 import RealCalculator.Function;
+//Class for interpreting Strings into mathematical statements and simplifying them
+//. Supports in paranthesis order of operations, i.e. 1+(3*2) will produce an 
+//output of 7, but does not yet support operator based order of operations, 
+//i.e. 1+3*2 will produce an output of 8. 
 
 public class TextParser {
+
   public static String toParse;
   static ArrayList<Double> function;
   static int set = 0;
-  static char[] digitsList = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
-      '9' };
+  
+  
+  // Viable characters that can be used in digits string
   static ArrayList<Character> digits = new ArrayList<Character>();
 
   public TextParser(String s) {
@@ -25,33 +31,41 @@ public class TextParser {
     digits.add('9');
     digits.add('.');
   }
+
+  // Calls inspectParanthesis method, simplifies call of inspectParanthesis
+  // as this method has no arguments
   public double parse() throws Exception {
     return inspectParanthesis(0, toParse.length());
   }
 
+  // Method that simplifies mathematical statement between characters specified
+  // in
+  // arguments using recursion.
   public double inspectParanthesis(int first, int last) throws Exception {
     int setFirst = 0, setLast = 0;
     int r = 0;
     double solution = 0;
     boolean numFound = false;
     int func = 1;
+    // Tests for edge case if there is no input
     if (first == last) {
       String answer = "";
       return Double.parseDouble(answer + toParse.charAt(first));
     } else {
       for (r = first; r < last; r++) {
+        // edge case for if multiplication is attempted without * operator,
+        // i.e. 8(8)
         if (toParse.charAt(r) == '(') {
-          System.out.println("woah1");
-          if (r>0) {
-            System.out.println("woah");
-            if (toParse.charAt(r-1)==')'||digits.contains(toParse.charAt(r-1))){
-              func=3;
-              System.out.println("wow");
+          if (r > 0) {
+            if (toParse.charAt(r - 1) == ')'
+                || digits.contains(toParse.charAt(r - 1))) {
+              func = 3;
             }
           }
           int findPara = 0;
           r++;
           setFirst = r;
+          // Finds and matches pairs of parenthesis
           while (toParse.charAt(r) != ')' || findPara > 0) {
             if (r > last) {
               throw new Exception("Not enough paranthesis");
@@ -69,71 +83,94 @@ public class TextParser {
             }
           }
           setLast = r;
-          if (func == 1) {
-            solution += inspectParanthesis(setFirst, setLast);
-          } else if (func == 2) {
-            solution -= inspectParanthesis(setFirst, setLast);
-          } else if (func == 3) {
-            solution *= inspectParanthesis(setFirst, setLast);
-          } else if (func == 4) {
-            solution /= inspectParanthesis(setFirst, setLast);
-          } else if (func == 5) {
-            double bounds= inspectParanthesis(setFirst,setLast);
-            solution=Math.pow(solution, bounds);
-          }
+          solution = determineFuncRecursively(func, solution, setFirst,
+              setLast);
           if (r + 1 != toParse.length()) {
             r++;
           }
         }
-        if (toParse.charAt(r) == '+') {
-          func = 1;
-        }
-        if (toParse.charAt(r) == '-') {
-          func = 2;
-        }
-        if (toParse.charAt(r) == '*') {
-          func = 3;
-        }
-        if (toParse.charAt(r) == '/') {
-          func = 4;
-        }
-        if (toParse.charAt(r) == '^') {
-          func = 5;
-        }
+        func = determineFunc(func, toParse.charAt(r));
         if (digits.contains(toParse.charAt(r)) == true) {
-          String number = "";
-          int numOfDots = 0;
-          while (digits.contains(toParse.charAt(r)) == true) {
-            number += toParse.charAt(r);
-            if (toParse.charAt(r) == '.') {
-              numOfDots++;
-            }
-            if (numOfDots > 1) {
-              throw new Exception("More than one decimal point.");
-            }
-            if (r < toParse.length() - 1
-                && digits.contains(toParse.charAt(r + 1))) {
-              r++;
-            } else {
-              break;
-            }
-          }
+          String number = determineNum(r);
+          r += number.length()-1;
           double include = Double.parseDouble(number);
-
-          if (func == 1) {
-            solution += include;
-          } else if (func == 2) {
-            solution -= include;
-          } else if (func == 3) {
-            solution *= include;
-          } else if (func == 4) {
-            solution /= include;
-          } else if (func == 5) {
-            solution=Math.pow(solution, include);
-          }
+          solution = resolveFunc(func, solution, include);
         }
       }
 
+    }
+    return solution;
+  }
+  //Method to find a string of numbers
+  public String determineNum(int r) throws Exception {
+    String number = "";
+    int numOfDots = 0;
+    while (digits.contains(toParse.charAt(r)) == true) {
+      number += toParse.charAt(r);
+      if (toParse.charAt(r) == '.') {
+        numOfDots++;
+      }
+      if (numOfDots > 1) {
+        throw new Exception("More than one decimal point.");
+      }
+      if (r < toParse.length() - 1 && digits.contains(toParse.charAt(r + 1))) {
+        r++;
+      } else {
+        break;
+      }
+    }
+    return number;
+  }
+  //Method calls inspectParenthesis() between pairs of parenthesis, recursively
+  //solving equation
+  public double determineFuncRecursively(int func,double solution,int setFirst, int setLast) throws Exception {
+    
+    if (func == 1) {
+      solution += inspectParanthesis(setFirst, setLast);
+    } else if (func == 2) {
+      solution -= inspectParanthesis(setFirst, setLast);
+    } else if (func == 3) {
+      solution *= inspectParanthesis(setFirst, setLast);
+    } else if (func == 4) {
+      solution /= inspectParanthesis(setFirst, setLast);
+    } else if (func == 5) {
+      double bounds= inspectParanthesis(setFirst,setLast);
+      solution=Math.pow(solution, bounds);
+    }
+    return solution;
+  }
+  //Method returns the operator to be used based on char argument
+  public int determineFunc(int funcIn,char op) {
+    int func=funcIn;
+    if (op == '+') {
+      func = 1;
+    }
+    if (op == '-') {
+      func = 2;
+    }
+    if (op == '*') {
+      func = 3;
+    }
+    if (op == '/') {
+      func = 4;
+    }
+    if (op == '^') {
+      func = 5;
+    }
+    return func;
+  }
+  //Method resolves function 
+  public double resolveFunc(int func, double solution, double include) {
+    if (func == 1) {
+      solution += include;
+    } else if (func == 2) {
+      solution -= include;
+    } else if (func == 3) {
+      solution *= include;
+    } else if (func == 4) {
+      solution /= include;
+    } else if (func == 5) {
+      solution=Math.pow(solution, include);
     }
     return solution;
   }
